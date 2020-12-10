767a0ec0a8c9fbf0a6db410ed0a58f01d26fb79b
Mohamed <malbgal@gmail.com>
2020-12-09 17:25:05 -0800

    purgecss issue fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    purge: [
-      "./pages/**/*.vue", 
-      "./components/**/*.vue", 
-      "./plugins/**/*.vue",
-      "./static/**/*.vue",
-      "./store/**/*.vue"
-    ],
-    theme: {},
-    variants: {},
-    plugins: []
-  };
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
