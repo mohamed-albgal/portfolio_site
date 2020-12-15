4f8823c7a1a46bf4810b1ec227f5c9579b17d33b
Mohamed <malbgal@gmail.com>
2020-12-15 03:27:09 -0800

    minor change

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,7 +3,7 @@ import {title, tagLine, body} from '../../content/home.yaml'
 import Container from "../components/Container"
 export default () => {
     return (
-        <Container heading={title} subHeading={tagLine}>
+        <Container heading={title} subHpwdeading={tagLine}>
             <div className="text-lg mt-10 text-gray-500">{body}</div>
         </Container>
     )
