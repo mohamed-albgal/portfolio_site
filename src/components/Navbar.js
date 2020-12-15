bdcb4f61d3b114b483604348a0755de1fe768110
Mohamed <malbgal@gmail.com>
2020-12-15 00:15:20 -0800

    read home page from yaml

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import self from '../images/self-image.jpg'
 import {Link} from 'gatsby';
+import {name, links} from '../../content/nav.yaml'
 const NavBar = () => {
     return (
         <header className="mt-4 container">
@@ -10,12 +11,10 @@ const NavBar = () => {
                     <img src={self} className="  rounded-lg object-contain"></img>
                 </div>
                 <div id="name" className="inline-block text-2xl font-extrabold ml-10  uppercase tracking-widest">
-                    <p className="p-8 text-logo-darkBlue">Mohamed Albgal</p>
+                    <p className="p-8 text-logo-darkBlue">{name}</p>
                     <div id="tags" className="flex pl-5">
-                    <Link to="/skills" className="px-4 font-medium hover:text-logo-darkBlue  text-logo-lightIndigo text-sm uppercase tracking-widest">Skills</Link>
-                    <Link to="/projects" className="px-4 font-medium hover:text-logo-darkBlue  text-logo-lightIndigo text-sm uppercase tracking-widest">Projects</Link>
-                    <Link to="/contact" className="px-4 font-medium hover:text-logo-darkBlue  text-logo-lightIndigo text-sm uppercase tracking-widest">Contact</Link>
-
+                    <Link to="/" className="px-4 font-medium hover:text-logo-darkBlue  text-logo-lightIndigo text-sm uppercase tracking-widest">home</Link>
+                    {links.split(',').map(link => <Link to={`/${link}`} className="px-4 font-medium hover:text-logo-darkBlue  text-logo-lightIndigo text-sm uppercase tracking-widest">{link}</Link>)}    
                     </div>
                 </div>
             </div>
