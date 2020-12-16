28d5ed37a62a42bb019c1d379efc30aa2be9fe25
Mohamed <malbgal@gmail.com>
2020-12-16 03:31:49 -0800

    branch finished

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,13 +4,16 @@ import logo from '../images/logo_malb.svg'
 import { resumeURL} from '../../content/home.yaml'
 import {name, links} from '../../content/nav.yaml'
 
-
+const fjlds = <div className="inline-block h-10">
+<a href="linkedin.com/mohamed-albgal" target="_blank"></a>
+    <img src="./linkedin_logo" className="object-fit"/>
+<a href="gihub.com/mohamed-albgal" target="_blank">
+    <img src="./github_logo"/>
+</a>
+</div>
 
 const Layout = ({ children }) => {
     return (
-        // <div style={{margin:`3rem auto`, maxWidth:650, padding:`0 1rem`}}>
-        //     { children }
-        // </div>
         <div>
             <div className="bg-gray-50 shadow-2xl flex justify-between flex-wrap pt-10">
                 <Link className="" to="/">
@@ -20,6 +23,7 @@ const Layout = ({ children }) => {
                 </Link>
                 <div className=" inline-block   self-center mt-14 flex-shrink-0">
                     <p className=" text-logo-darkBlue text-4xl sm:mb-10 font-extrabold text-center  uppercase tracking-widest">{name}</p>
+                    
                     <div id="tags" className="flex">
                         <Link to="/" activeClassName="activeNavLink" className="px-12 py-1 font-medium  text-logo-lightIndigo text-sm uppercase tracking-widest">about</Link>
                         {links.split(',').map(link => <Link to={`/${link}`} activeClassName="activeNavLink" className="px-12 py-1 font-medium  text-logo-lightIndigo text-sm uppercase tracking-widest">{link}</Link>)}    
